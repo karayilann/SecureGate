5093d298a3d2ed8c206dbda58fbed369917ef44a
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2026-09-12 19:28:49 +0300

    Create IPlanRepository.cs

---
+++ SecureGate.Domain/Interfaces/IPlanRepository.cs
@@ -0,0 +1,10 @@
+﻿using SecureGate.Domain.Entities;
+using SecureGate.Domain.Enums;
+
+namespace SecureGate.Domain.Interfaces
+{
+    public interface IPlanRepository
+    {
+        Task<Plan?> GetByTypeAsync(PlanType planType);
+    }
+}
